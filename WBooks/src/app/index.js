9aca7ccba45dfe9728224e67d8c24526221622a1
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-05-08 11:29:14 -0300

    Add react-navigation, add withNavigation into Book Component to wrapp Book and add the navigation prop, create BookDetail empty

--- WBooks/src/app/index.js
+++ WBooks/src/app/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import { Library } from './screens/Library';
-import { BookDetail } from './screens/BookDetail';
+import { ROUTES } from '../constants/routes';
+
+import Library from './screens/Library';
+import BookDetail from './screens/BookDetail';
+
+const LibraryNavigator = createStackNavigator({
+  [ROUTES.Library]: {
+    screen: Library
+  },
+  [ROUTES.BookDetail]: {
+    screen: BookDetail
+  }
+});
+
+const AppNavigator = createAppContainer(LibraryNavigator);
 
 function MyApp() {
-  return <LibraryNavigator />;
+  return <AppNavigator />;
 }
-
-const LibraryNavigator = createStackNavigator(
-  {
-    Library: {
-      screen: Library
-    },
-    BookDetailScreen: {
-      screen: BookDetail
-    }
-  },
-  {
-    initialRouteName: 'Library'
-  }
-);
 
 export default MyApp;
--- WBooks/src/app/screens/Library/components/Book/index.js
+++ WBooks/src/app/screens/Library/components/Book/index.js
@@ -1,34 +1,48 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import PropTypes from 'prop-types';
+import { withNavigation } from 'react-navigation';
 
 import imageNoExists from '../../../../../assets/general/no_book_question.jpg';
+import { ROUTES } from '../../../../../constants/routes';
+import { navigationProptype } from '../../../../../propTypes/navigation';
 
 import styles from './styles';
 
-function Book({ image, title, author }) {
-  const imageSource = image ? { uri: image } : imageNoExists;
-  return (
-    <View style={styles.bookContainer}>
-      <View style={styles.bookImage}>
-        <Image style={styles.image} source={imageSource} />
-      </View>
-      <View style={styles.titleAndAuthor}>
-        <View>
-          <Text style={styles.title}>{title}</Text>
+class Book extends Component {
+  handlePress = () => {
+    const {
+      navigation: { navigate }
+    } = this.props;
+    navigate(ROUTES.BookDetail);
+  };
+
+  render() {
+    const { image, title, author } = this.props;
+    const imageSource = image ? { uri: image } : imageNoExists;
+    return (
+      <TouchableOpacity style={styles.bookContainer} onPress={this.handlePress}>
+        <View style={styles.bookImage}>
+          <Image style={styles.image} source={imageSource} />
         </View>
-        <View style={styles.authorContainer}>
-          <Text style={styles.author}>{author}</Text>
+        <View style={styles.titleAndAuthor}>
+          <View>
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.authorContainer}>
+            <Text style={styles.author}>{author}</Text>
+          </View>
         </View>
-      </View>
-    </View>
-  );
+      </TouchableOpacity>
+    );
+  }
 }
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  image: PropTypes.string
+  image: PropTypes.string,
+  navigation: navigationProptype.isRequired
 };
 
-export default Book;
+export default withNavigation(Book);
