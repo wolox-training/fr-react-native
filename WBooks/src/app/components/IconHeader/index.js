68038b33851dea3b930ebc39d0becc1acb6f1f49
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-05-10 14:36:25 -0300

    Add constants folder into iconHeader component

--- WBooks/src/app/components/IconHeader/index.js
+++ WBooks/src/app/components/IconHeader/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { TouchableOpacity, Image } from 'react-native';
 
-import bell from '../../../assets/navigation_bar/ic_notifications.png';
-import search from '../../../assets/navigation_bar/ic_search.png';
-
 import styles from './styles';
+import { icons } from './constants/iconsName';
 
-const icons = {
-  bell,
-  search
-};
 function IconHeader({ icon, action, style }) {
   const particularIcon = icons[icon];
   return (
