1e01c40ae80e6385aa76f9b63478e4b103c38f52
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-05-17 09:15:25 -0300

    DetailBox change

--- WBooks/src/app/screens/BookDetail/components/DetailBox/index.js
+++ WBooks/src/app/screens/BookDetail/components/DetailBox/index.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { withNavigation } from 'react-navigation';
 
-import { genericBook } from './constants/genericBook';
-import { isAvailable } from './utils/isAvailable';
-import { availability } from './constants/text';
-import styles from './styles';
+
 import DetailBoxLayout from './layout';
 
 class DetailBox extends Component {
@@ -16,20 +13,13 @@ class DetailBox extends Component {
     const { navigation } = this.props;
     const book = navigation.getParam('book', {});
     const { image_url: image, title, author, year } = book;
-    const availabilityStyle = isAvailable(styles.available, styles.notAvailable);
-    const availabilityText = isAvailable(availability.available, availability.notAvailable);
-    const rentButtonStyle = isAvailable(styles.rentButtonAvailable, styles.rentButtonNotAvailable);
-    const disabled = !genericBook.isAvailable;
+
     return (
       <DetailBoxLayout
         image={image}
         title={title}
         author={author}
         year={year}
-        availabilityStyle={availabilityStyle}
-        availabilityText={availabilityText}
-        rentButtonStyle={rentButtonStyle}
-        disabled={disabled}
         handleRentBook={this.handleRentBook}
         handleAddToWishlist={this.handleAddToWishlist}
       />
