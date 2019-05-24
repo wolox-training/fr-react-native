29b0d2e1cfb3651bfa465a3ee1989b354f5d86a3
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-05-24 14:55:14 -0300

    add reselect books

--- WBooks/src/app/screens/Search/index.js
+++ WBooks/src/app/screens/Search/index.js
@@ -2,27 +2,24 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
 
+import { getBooksSelector } from './utils/selector';
 import SearchComposed from './composition';
 import styles from './styles';
 
 class Search extends Component {
-  filterBooks = (text, books) =>
-    books.filter(book => book.title.toLowerCase().includes(text.toLowerCase())).sort();
-
   render() {
-    const { text, books } = this.props;
-    const data = this.filterBooks(text, books);
+    const { books, text } = this.props;
     return (
       <View style={styles.container}>
-        <SearchComposed data={data} text={text} />
+        <SearchComposed data={books} text={text} />
       </View>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  text: state.search.text,
-  books: state.books.books
+  books: getBooksSelector(state),
+  text: state.search.text
 });
 
 export default connect(mapStateToProps)(Search);
---
+++ WBooks/src/app/screens/Search/utils/selector.js
@@ -0,0 +1,15 @@
+import { createSelector } from 'reselect';
+import Immutable from 'seamless-immutable';
+
+const getBooks = state => state.books.books;
+const getText = state => state.search.text;
+
+export const getBooksSelector = createSelector(
+  getBooks,
+  getText,
+  (books, text) => {
+    return Immutable.asMutable(
+      books.filter(book => book.title.toLowerCase().includes(text.toLowerCase()))
+    ).sort(book => book.id);
+  }
+);
