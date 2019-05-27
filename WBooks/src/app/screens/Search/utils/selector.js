19198420f1e86534b42e7283503774abb04aad3d
Felipe Rodriguez <frodriguez@Woloxs-Mac-mini.local>
2019-05-27 15:28:25 -0300

    Resolve pr

--- WBooks/src/app/screens/Search/utils/selector.js
+++ WBooks/src/app/screens/Search/utils/selector.js
@@ -7,11 +7,10 @@ const getText = state => state.search.text;
 export const getBooksSelector = createSelector(
   getBooks,
   getText,
-  (books, text) => {
-    return Immutable.asMutable(
-      books.filter(book => book.title.toLowerCase().includes(text.toLowerCase()))
-    ).sort(book => book.id);
-  }
+  (books, text) =>
+    Immutable.asMutable(books.filter(book => book.title.toLowerCase().includes(text.toLowerCase()))).sort(
+      book => book.id
+    )
 );
 export const getEmptyInputSelector = createSelector(
   getText,
